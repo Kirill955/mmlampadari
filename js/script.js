fd862cc6c11bed1da61d3a0f890bdf682ea7ed92
Кирилл Омаров <kirill.kirill.omarov@mail.ru>
2020-04-30 07:51:56 +0600

    Add main, start footer.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.main__slider').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "slick-dots",
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    })
+})
