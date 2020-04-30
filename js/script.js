af0e019882e98d2164674dd178874136449e33f0
Кирилл Омаров <kirill.kirill.omarov@mail.ru>
2020-04-30 23:23:55 +0600

    Add footer, modal window, animation

--- js/script.js
+++ js/script.js
@@ -8,4 +8,21 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1
     })
+    /* Modal window */
+    let btnModal = $('.header__menu-serviz-text'),
+    modalBackground = $('.modal__background'),
+    modal = $('.modal'),
+    closeModal = $('.modal-close');
+
+    btnModal.on('click', function (event) {
+        event.preventDefault()
+        modalBackground.addClass('show')
+
+        modalBackground.on('click', function (e) {
+            if( !modal.is(e.target) && modalBackground.has(e.target).length === 0 
+            || e.target == closeModal){
+                modalBackground.removeClass('show');
+            } else {}
+        });
+    });
 })
